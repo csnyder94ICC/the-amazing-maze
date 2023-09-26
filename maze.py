cad0c80eca39a78a32176330ffdd09b0899c4fee
Joshua Carroll <143561153+Pythooons@users.noreply.github.com>
2023-09-26 18:39:57 -0400

    Update maze.py
    Player choices

--- maze.py
+++ maze.py
@@ -41,3 +41,56 @@ def load_game_state():
 
 # Define the game loop below here
 
+# Game loop
+while not game_over:
+    room = rooms[current_room]
+
+    # Displays Samanthas room Descriptions and choices
+    print(room['description'])
+    for i, choice in enumerate(room['choices']):
+        print(f'{i + 1}. {choice}')
+
+    # What the player chooses
+    choice_num = input('Enter your choice: ')
+
+    # Code to handle player choice
+    if choice_num.isdigit():
+        choice_num = int(choice_num)
+        if 1 <= choice_num <= len(room['choices']):
+            choice = room['choices'][choice_num - 1]
+            if choice == 'Exit game':
+                game_over = True
+            elif current_room == 'start' and choice == 'Enter the maze':
+                current_room = 'room1'
+            elif current_room == 'room1':
+                if choice == 'Go through the left door':
+                    current_room = 'room2'
+                elif choice == 'Go through the middle door':
+                    current_room = 'treasure_room'
+                elif choice == 'Go through the right door':
+                    current_room = 'room2'
+                elif choice == 'Go back':
+                    current_room = 'start'
+            elif current_room == 'room2':
+                if choice == 'Go left':
+                    current_room = 'exit'
+                elif choice == 'Go right':
+                    current_room = 'treasure_room'
+                elif choice == 'Go back':
+                    current_room = 'room1'
+            elif current_room == 'treasure_room':
+                if choice == 'Take some treasure':
+                    current_room = 'room2'
+                elif choice == 'Leave the treasure room':
+                    current_room = 'room1'
+                elif choice == 'Go back':
+                    current_room = 'room1'
+            elif current_room == 'exit':
+                if choice == 'Exit game':
+                    game_over = True
+    else:
+        print("Invalid input. Please enter a valid choice number.")
+
+
+# The only thing really left is saving the game state if someone wants to handle that.
+# I will debug once the code is completed and Samantha inputs her parts.
